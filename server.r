9f7b6341dc32b314b60d3319fc66b4f3849aa12a
bertoli12 <peter.j.bertoli@gmail.com>
2015-01-29 18:21:03 -0500

    Summary
    Data for App

--- server.r
+++ server.r
@@ -1,13 +1,10 @@
 library(shiny)
-library(RSQLite)
-library(RODBC)
 library(ggvis)
-library(jpeg)
-library(png)
 library(RANN)
 library(data.table)
 
-dt.raw <- as.data.table(read.csv('L:\\public\\share\\hcms\\Analyst-Shared\\Peter Bertoli\\Garbage FIles\\Shiny\\Summary.csv'))
+dt.raw <- as.data.table(read.csv("Summary.csv"))
+PlayerNames <- as.character(dt.raw$Player)
 
 shinyServer(function(input, output, session) {
   
@@ -20,64 +17,69 @@ shinyServer(function(input, output, session) {
     Player.tt <- all_Player[all_Player$Player == x$Player, ]
     
     paste0("<b>",Player.tt$Player,"</b><br>",
-           Player.tt$Team)  
+           "Team: ",Player.tt$Team,"<br>",
+           "Position: ",Player.tt$Position,"<br>",
+           "Salary: ",Player.tt$Salary,"<br>"
+           )  
   }
   
   
   ### Cluster Analysis ###
   dt.cluster.cln <- reactive({
-      if (is.null(input$Player))
-        return()
+    input$goButton
     
-      Player.test <- input$Player
-      metric.list <- as.character(input$metrics)
-      metric.list <- append(as.character(list('Player','Team')),metric.list)
-      n <- input$matches
-      
-      dt.query <- subset(dt.raw, select = c(metric.list))   ### Create Data Frames of Selected Metrics
-      dt.test <- dt.query[Player.test == Player]
-      dt.test$Player <- NULL
-      dt.test$Team <- NULL
-      dt.query.cln <- dt.query[Player.test != Player]
-      dt.query.cln$Player <-NULL
-      dt.query.cln$Team <-NULL
-      
-      cluster <- nn2(dt.query.cln,query=dt.test,k=n+1)   ### Run Cluster
-      
-      matches <- as.data.frame(cluster$nn.idx)   ### Get Nearest Neighbors
-      matches.cln <- matches[1,]
-      names(matches.cln) <- NULL
-      matches.cln <- unlist(c(matches.cln))
-      
-      distance <- as.data.frame(cluster$nn.dists)   ### Get Distances
-      distance.cln <- distance[1,]
-      names(distance.cln) <- NULL
-      distance.cln <- unlist(c(distance.cln))
-      
-      dt.test <- dt.query[Player.test == Player]   ### Start Final Output
-      dt.query.cln <- dt.query[Player.test != Player]
-     
-      for(i in matches.cln){
-        dt.test <- rbind(dt.test,dt.query[i:i,])} 
-      
-      row <- 2
-      dt.test$Distance[1] <- 0
-
-      for(i in distance.cln){
-        dt.test$Distance[row] <- i
-        row <- row + 1}
-
-      for(i in 2:nrow(dt.test)-1){        
-        player.row <- i      
-        if (dt.test$Player[player.row] == Player.test & dt.test$Distance[player.row] > 0){
-          dt.test <- dt.test[-c(player.row), ]
-        }
-      }
-      
-      dt.cluster <- dt.test
+    isolate({
+        Player.test <- input$Player
+        metric.list <- append(as.character(list('Player','Team','Position','Salary')),as.character(input$metrics))
+        n <- input$matches
+        
+        dt.query <- subset(dt.raw, select = c(metric.list))   ### Create Data Frames of Selected Metrics
+        dt.test <- dt.query[Player.test == Player]
+        dt.test$Player <- NULL
+        dt.test$Team <- NULL
+        dt.test$Position <- NULL
+        dt.test$Salary <- NULL
+        dt.query.cln <- dt.query[Player.test != Player]
+        dt.query.cln$Player <-NULL
+        dt.query.cln$Team <-NULL
+        dt.query.cln$Position <-NULL
+        dt.query.cln$Salary <-NULL
+        
+        cluster <- nn2(dt.query.cln,query=dt.test,k=n+1)   ### Run Cluster
+        
+        matches <- as.data.frame(cluster$nn.idx)   ### Get Nearest Neighbors
+        matches.cln <- matches[1,]
+        names(matches.cln) <- NULL
+        matches.cln <- unlist(c(matches.cln))
+        
+        distance <- as.data.frame(cluster$nn.dists)   ### Get Distances
+        distance.cln <- distance[1,]
+        names(distance.cln) <- NULL
+        distance.cln <- unlist(c(distance.cln))
+        
+        dt.test <- dt.query[Player.test == Player]   ### Start Final Output
+        dt.query.cln <- dt.query[Player.test != Player]
+       
+        for(i in matches.cln){
+          dt.test <- rbind(dt.test,dt.query[i:i,])} 
+        
+        row <- 2
+        dt.test$Distance[1] <- 0
+  
+        for(i in distance.cln){
+          dt.test$Distance[row] <- i
+          row <- row + 1}
+  
+        for(i in 2:nrow(dt.test)-1){        
+          player.row <- i      
+          if (dt.test$Player[player.row] == Player.test & dt.test$Distance[player.row] > 0){
+            dt.test <- dt.test[-c(player.row), ]
+          }
+        }  
+        dt.cluster <- dt.test
     })
-  
-  
+  })
+   
     ### Metrics List Output ###
   
     output$xvar <- renderUI({
@@ -90,18 +92,20 @@ shinyServer(function(input, output, session) {
   
     ### GGVIS Plot ###  
     PlayerChart <- reactive({
-      if (is.null(input$Player))
+      if (is.null(dt.cluster.cln()))
         return()
       
+      metric.list <- append(as.character(list('Player','Team','Salary','Position')),as.character(input$metrics))      
+                         
       xvar_name <- names(metric.list)[metric.list == input$xvar]
       yvar_name <- names(metric.list)[metric.list == input$yvar]
-
-      xvar <- prop("x", as.symbol(input$xvar))
-      yvar <- prop("y", as.symbol(input$yvar))      
+      
+      x.axis <- prop("x", as.symbol(input$xvar))
+      y.axis <- prop("y", as.symbol(input$yvar))  
       
       dt.cluster.cln %>%
-        ggvis(x = xvar, 
-              y = yvar) %>%
+        ggvis(x = x.axis, 
+              y = y.axis) %>%
         layer_points(size := 75, 
                      size.hover := 500,
                      size.brush := 500,
@@ -109,10 +113,10 @@ shinyServer(function(input, output, session) {
                      fillOpacity.hover := 0.8,
                      fillOpacity.brush := 0.8,
                      key := ~Player,
-                     stroke = ~Team,
+                     stroke = ~Position,
                      strokeWidth := 3) %>%
         add_axis("x", title = "") %>%
-        add_legend("stroke", title = "Team") %>%
+        add_legend("stroke", title = "Position") %>%
         add_axis("x", 
                  title = xvar_name,
                  title_offset = 50) %>%
@@ -122,12 +126,15 @@ shinyServer(function(input, output, session) {
         add_tooltip(Player_tooltip,"hover") %>%
         add_axis("y", title = "") %>%
         set_options(hover_duration = 200) %>%
-        set_options(width = 1000, height = 450)
-    })
-  
+        set_options(width = 1000, height = 450)    
+    })  
+
     PlayerChart %>% bind_shiny("PlayerChart")
     
     output$cluster.tbl <- renderDataTable({
+      if (is.null(dt.cluster.cln()))
+        return()
+      
       data <- dt.cluster.cln()
     },options = list(paging = FALSE))
   
--- ui.R
+++ ui.R
@@ -1,42 +1,63 @@
 library(shiny)
-library(RSQLite)
-library(RODBC)
 library(ggvis)
-library(jpeg)
-library(png)
 library(RANN)
 library(data.table)
 
-dt.raw <- as.data.table(read.csv('L:\\public\\share\\hcms\\Analyst-Shared\\Peter Bertoli\\Garbage FIles\\Shiny\\Summary.csv'))
+
+dt.raw <- as.data.table(read.csv("Summary.csv"))
+
 PlayerNames <- as.character(dt.raw$Player)
 metricsToInclude <- as.character(colnames(dt.raw))
-metricsToInclude <- metricsToInclude[metricsToInclude != 'Team' & metricsToInclude != 'Player']
+metricsToInclude <- metricsToInclude[metricsToInclude != 'Team' 
+                                     & metricsToInclude != 'Player'
+                                     & metricsToInclude != 'Salary'
+                                     & metricsToInclude != 'Position']
 
 shinyUI(
-  fluidPage(
-    titlePanel(h1(''),
-               h1('')
+  fluidPage(theme = "bootstrap.css",
+    titlePanel(h1(tags$p(class="text-primary",tags$strong('NBA Replacement Player Locator'))),
+               'NBA Player Finder'
                ),
     sidebarLayout(
         sidebarPanel(
+        tags$head(
+          tags$style(type="text/css", "select { width: 500px; }"),
+          tags$style(type="text/css", ".col-sm-4 { width: 25%; }")
+          ),
+        h4('Use the inputs below to select desired metrics and hit the refresh button to update. Hover over the data in the graph for more info:'),
+        br(),
+        tags$button(class="btn btn-danger",id="goButton",class="btn action-button shiny-bound-input","Refresh"),
+        br(),
+        h3(''),
+        selectInput('Player', h4(tags$p(class="text-danger",'Player Name: ')), c(Choose='', PlayerNames), selected = "Robert Covington"),
+        h4(''),
         selectInput('metrics', 
-                    'Metrics to Include: ',
+                    h4(tags$p(class="text-danger",'Metrics to Include: ')),
                     c(Choose= '',metricsToInclude), 
                     multiple=TRUE, 
                     selectize=TRUE, 
-                    selected =  list('Player','GameAvgPTS','GameAvgBLK')),
-        selectInput("xvar", "X-axis variable",metricsToInclude, selected = "GameAvgPTS"),
-        selectInput("yvar", "Y-axis variable",metricsToInclude, selected = "GameAvgPTS"),
-        uiOutput("xvar"),
-        uiOutput("yvar"), 
-        sliderInput("matches", "Select Number of Neighbors:", 
-                    min = 0, max = 50, value = 10, step= 1),
-        selectInput('Player', 'Player Name: ', c(Choose='', PlayerNames), selectize=TRUE, selected = "James Harden")
+                    selected =  list('Player','GameAvgPTS','GameAvgAST','GameAvgMin','Age')),
+        selectInput("xvar", h4(tags$p(class="text-danger",'X-Axis Variable: ')),choices = append(as.character(list('Distance')),metricsToInclude), selected = "Distance"),
+        selectInput("yvar", h4(tags$p(class="text-danger",'Y-Axis Variable: ')),choices = append(as.character(list('Distance')),metricsToInclude), selected = "GameAvgPTS"), 
+        sliderInput("matches", h4(tags$p(class="text-danger",'Number of Player Matches: ')), 
+                    min = 0, max = 50, value = 20, step= 1),
+        br(),        
+        h4(tags$p(class="text-danger",'Created by Peter Bertoli')),
+        HTML("<a href = https://github.com/bertoli12/NBA-Player-Replacement-Shiny-App target=_blank> Source Code </a>")
       ),
     mainPanel(
-      textOutput("output.grouping.list.cln"),
+      HTML('<div class="alert alert-dismissable alert-info">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <h4>This tool is designed to allow users to find players of similiar caliber based on 2014 YTD game averages of the user input metrics. It uses a dynamic 
+            <a href="http://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm" class="alert-link">
+            nearest neighbor cluster</a> algorithm. We recognize that there are some bugs with the axis selection and are working to fix them. Please contact <a href="mailto:peter.j.bertoli@gmail.com?subject=NBA Shiny App" class="alert-link">
+            Peter</a> for questions.<h4>
+            </div>'),
+      br(),
+      h3('Replacement Player Graph'),
       ggvisOutput("PlayerChart"),
-      h2(''),
+      br(),
+      h3('Searchable Data Table'),
       dataTableOutput(outputId="cluster.tbl")
      )
     )
